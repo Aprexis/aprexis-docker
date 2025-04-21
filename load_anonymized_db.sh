0c8ce8f1c3f416107bb843137914352219b4b915
Ian Brown <ibrown@bear-code.com>
2025-04-21 09:39:11 -0400

    Allow for the source folder for anonymized data to be overridden.

--- load_anonymized_db.sh
+++ load_anonymized_db.sh
@@ -6,6 +6,7 @@
 # Environment:
 #    APREXIS_VARIETY              the version of Aprexis to run (api or platform).
 #    APREXIS_DOCKER_COMPOSE_FILE  the docker compose file to use to build the system. Default depends on APREXIS_VARIETY.
+#    APREXIS_DATA_FOLDER          the folder containing the database dump to load. Default is ./aprexis-data/anonymized.
 
 # Author:  Ian Brown
 # Since:   2024/08/23
@@ -14,7 +15,8 @@
 SHELL_DIR=`dirname "$0"`
 APREXIS_VARIETY=${APREXIS_VARIETY:-api}
 APREXIS_DOCKER_COMPOSE_FILE=docker-compose-${APREXIS_VARIETY}.yml
-data_file=$(ls -t `find ./aprexis-data -type f ! -iname ".*"` | head -n 1 | cut -d' ' -f2)
+APREXIS_DATA_FOLDER=${APREXIS_DATA_FOLDER:-./aprexis-data/anonymized}
+data_file=$(ls -t `find ${APREXIS_DATA_FOLDER} -type f ! -iname ".*"` | head -n 1 | cut -d' ' -f2)
 
 if [ "${data_file}" = "" ]; then
   echo "No data file to load. Aborting..."
@@ -22,11 +24,11 @@ if [ "${data_file}" = "" ]; then
 end
 
 
-${SHELL_DIR}/start_db.sh
-${SHELL_DIR}/drop_db.sh --have-databases
-${SHELL_DIR}/create_db.sh --have-databases
-${SHELL_DIR}/load_anonymized_data.sh --have-databases
-${SHELL_DIR}/enable_users.sh --have-databases
-${SHELL_DIR}/prepare_test_db.sh --have-databases
+${SHELL_DIR}/start_db.sh APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
+${SHELL_DIR}/drop_db.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
+${SHELL_DIR}/create_db.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
+${SHELL_DIR}/load_anonymized_data.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE} APREXIS_DATA_FOLDER=${APREXIS_DATA_FOLDER}
+${SHELL_DIR}/enable_users.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
+${SHELL_DIR}/prepare_test_db.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
 
 ${SHELL_DIR}/down.sh
--- new_db.sh
+++ new_db.sh
@@ -6,21 +6,22 @@
 # Environment:
 #    APREXIS_VARIETY              the version of Aprexis to run (api or platform).
 #    APREXIS_DOCKER_COMPOSE_FILE  the docker compose file to use to build the system. Default depends on APREXIS_VARIETY.
+#    APREXIS_DATA_FOLDER          the folder containing the database dump to load. Default is ./aprexis-data/database.
 
 # Author:  Ian Brown
 # Since:   2021/05/21
-# Version: 1.0.0
+# Version: 1.1.0
 
 SHELL_DIR=`dirname "$0"`
 APREXIS_VARIETY=${APREXIS_VARIETY:-api}
 APREXIS_DOCKER_COMPOSE_FILE=docker-compose-${APREXIS_VARIETY}.yml
+APREXIS_DATA_FOLDER=${APREXIS_DATA_FOLDER:-/aprexis-data/database}
 
-${SHELL_DIR}/start_db.sh
+${SHELL_DIR}/start_db.sh APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
 
-${SHELL_DIR}/drop_db.sh --have-databases
-${SHELL_DIR}/create_db.sh --have-databases
-${SHELL_DIR}/load_data.sh --have-databases
-${SHELL_DIR}/enable_users.sh --have-databases
-${SHELL_DIR}/prepare_test_db.sh --have-databases
-
-${SHELL_DIR}/down.sh
+${SHELL_DIR}/drop_db.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
+${SHELL_DIR}/create_db.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
+${SHELL_DIR}/load_data.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE} APREXIS_DATA_FOLDER=${APREXIS_DATA_FOLDER}
+${SHELL_DIR}/enable_users.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE} 
+${SHELL_DIR}/prepare_test_db.sh --have-databases APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
+${SHELL_DIR}/down.sh APREXIS_VARIETY=${APREXIS_VARIETY} APREXIS_DOCKER_COMPOSE_FILE=${APREXIS_DOCKER_COMPOSE_FILE}
